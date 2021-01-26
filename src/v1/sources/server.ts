95129bc07286ae8d09da8d474087e1eda64869b8
trabur <travis.burandt@gmal.com>
2021-01-26 14:01:22 -0600

    get check

--- src/v1/sources/server.ts
+++ src/v1/sources/server.ts
@@ -10,5 +10,5 @@ let config = { endpoint, version }
 export default function (app, channel) {
   app.post(`/${version}/${endpoint}/publish/:queue`, publish(channel, config))
   app.post(`/${version}/${endpoint}/consume/:queue`, publish(channel, config))
-  app.post(`/${version}/${endpoint}/check/:queue`, check(channel, config))
+  app.get(`/${version}/${endpoint}/check/:queue`, check(channel, config))
 }
