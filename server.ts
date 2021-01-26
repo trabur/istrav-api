623708246bb88ac39ed3b56ceab33ad2996bcd8a
trabur <travis.burandt@gmal.com>
2021-01-25 19:06:57 -0600

    publish 5

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import express from "express"
 import v1 from "./src/v1/app"
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 // cors
 app.use(function(req, res, next) {
